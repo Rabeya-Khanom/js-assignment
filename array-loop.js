59209b631e8d30d41044148aa3eb67a7ad8692c5
Rabeya-Khanom <rabeyakhanom33@gmail.com>
2024-10-16 14:02:25 +0600

    Add files via upload

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+//var number = [32,45,65, 56, 78, 56, 76, 45];
+//for(var i = 0; i<4; i++){
+  //  var element = numbers.length;[i];
+    //console.log(element);
+//}
+
+for(var laruGiven = 0; laruGiven<10; laruGiven++){
+    console.log("Rabeya");
+    console.log(laruGiven);
+
+}
---
+++ length.js
@@ -0,0 +1,4 @@
+const member =["Rakib","sakib","jakib","kamal",]
+ //const x= member.includes("Rakib")
+// console.log(x)
+console.log(Array.isArray(member));
